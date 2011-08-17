bf2c1633abaaa48a8b5a4e53d96307f15e7032cb
Valentin Robert <valentin.robert.42@gmail.com>
2011-08-18 01:41:12 +0200

    Added a few necessary things to xmonad.hs

--- xmonad.hs
+++ xmonad.hs
@@ -1,4 +1,63 @@
+import qualified Data.Map as M
 import XMonad
+import XMonad.Actions.TopicSpace
 import XMonad.Config.Azerty
+import XMonad.Hooks.ManageDocks
+import XMonad.Layout.Grid
+import XMonad.Layout.NoBorders
+import XMonad.Layout.PerWorkspace
+import XMonad.Util.Paste
 
-main = do xmonad $ azertyConfig
+myBrowser   = "chromium"
+myIM        = "pidgin"
+myTerminal  = "urxvt -depth 32 -fg white -bg black -sr -bc"
+
+myTopics = ["term", "web", "irc", "im"]
+
+myTopicConfig = TopicConfig
+    { defaultTopicAction    = const $ spawnTerminal >*> 3
+    , defaultTopic          = "term"
+    , maxTopicHistory       = 10
+    , topicDirs             = M.fromList $
+        []
+    , topicActions          = M.fromList $
+        [ ("web", spawnBrowser)
+        , ("irc", spawnIRC)
+        , ("im", spawnIM)
+        ]
+    }
+
+myKeys conf@(XConfig {modMask = modm}) = M.fromList $
+    [ ((modm,               xK_p), spawn "dmenu_run")
+    ]
+
+spawnBrowser    = spawn $ myBrowser
+spawnIM         = spawn $ myIM
+spawnIRC        = spawn $ "ssh -t pichon \"tmux attach -t irssi\""
+spawnTerminal   = spawn $ myTerminal
+
+tall_1_2 = Tall 1 (1/100) (50/100)
+tall_2_3 = Tall 1 (1/100) (65/100)
+
+myLayoutHook =
+    avoidStruts $
+    onWorkspaces ["irc", "web"] (noBorders  Full ||| tall_2_3) $
+    (Grid ||| tall_1_2)
+
+myConfig = do
+    checkTopicConfig myTopics myTopicConfig
+    return $ defaultConfig
+        { workspaces            = myTopics
+        , terminal              = myTerminal
+        , normalBorderColor     = "#0000ff"
+        , focusedBorderColor    = "#ff0000"
+        , modMask               = mod4Mask
+        , layoutHook            = myLayoutHook
+        , manageHook            = manageDocks
+        , keys                  =
+            \c -> myKeys c `M.union`
+                    azertyKeys c `M.union`
+                    keys defaultConfig c
+        }
+
+main = xmonad =<< myConfig
