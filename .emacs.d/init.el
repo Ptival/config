c0318aa2a2acba2883b119593c2446efcdcec292
Valentin Robert <valentin.robert.42@gmail.com>
2017-09-11 11:53:34 -0700

    slight tweaks to doom config?

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -31,32 +31,33 @@
 
 (doom! :feature
        evil            ; come to the dark side, we have cookies
-       jump            ; helping you navigate your code base
+       jump            ; helping you get around
        snippets        ; my elves. They type so I don't have to
        file-templates  ; auto-snippets for empty files
        hydra           ; keybindings that stick around
        spellcheck      ; tasing you for misspelling mispelling
-       syntax-checker  ; tasing you for every forgotten semicolon
+       syntax-checker  ; tasing you for every semicolon you forget
        version-control ; remember, remember that commit in November
-       workspaces      ; tab emulation, persistence and separate workspaces
+       workspaces      ; tab emulation, persistence & separate workspaces
        eval            ; repls, runners 'n builders; run code, run
-       ;debug           ; stepping through code, to help you add bugs
+      ;debug           ; FIXME stepping through code, to help you add bugs
 
        :completion
-       company         ; auto-completion backend
+       company         ; the ultimate code completion backend
        ivy             ; a search engine for love and life
-      ;helm
-      ;ido
+      ;helm            ; the *other* search engine for love and life
+      ;ido             ; the other *other* search engine...
 
        :ui
-       doom            ; doom-one; a look inspired by Atom's Dark One
+       doom            ; what makes DOOM look the way it does
        doom-dashboard  ; a nifty splash screen for Emacs
        doom-modeline   ; a snazzy Atom-inspired mode-line
        doom-quit       ; DOOM quit-message prompts when you quit Emacs
        hl-todo         ; highlight TODO/FIXME/NOTE tags
        nav-flash       ; blink the current line after jumping
        evil-goggles    ; display visual hints when editing in evil
-       unicode         ; extended unicode font support for various languages
+      ;unicode         ; extended unicode support for various languages
+      ;tabbar          ; FIXME an (incomplete) tab bar for Emacs
 
        :tools
        dired           ; making dired pretty [functional]
@@ -64,7 +65,7 @@
        eshell          ; a consistent, cross-platform shell (WIP)
        gist            ; interacting with github gists
        impatient-mode  ; show off code over HTTP
-      ;macos           ; macos-specific commands
+      ;macos           ; MacOS-specific commands
        neotree         ; a project drawer, like NERDTree for vim
        password-store  ; password manager for nerds
        prodigy         ; manage external services from within emacs
@@ -78,33 +79,33 @@
        ;cc              ; C/C++/Obj-C madness
        ;crystal         ; ruby at the speed of c
        ;csharp          ; unity, .NET, and mono shenanigans
-       data            ; config/data formats
+       ;data            ; config/data formats
        ;elixir          ; erlang done right
-       elm
+       ;elm             ; care for a cup of TEA?
        emacs-lisp      ; drown in parentheses
        ;go              ; the hipster dialect
        haskell         ; a language that's lazier than I am
        ;hy              ; readability of scheme w/ speed of python
        ;java            ; the poster child for carpal tunnel syndrome
-       javascript      ; all(hope(abandon(ye(who(enter(here))))))
+       ;javascript      ; all(hope(abandon(ye(who(enter(here))))))
        ;julia           ; a better, faster MATLAB
-       latex           ; writing papers in Emacs has never been so fun
+       ;latex           ; writing papers in Emacs has never been so fun
        ;lua             ; one-based indices? one-based indices
-       markdown        ; writing docs for people to ignore
-       ocaml           ; non-subjective camel
+       ;markdown        ; writing docs for people to ignore
+       ;ocaml           ; an objective camel
        ;perl            ; write code no one else can comprehend
        ;php             ; make php less awful to work with
        ;plantuml        ; diagrams for confusing people more
-       purescript
-       python          ; beautiful is better than ugly
+       ;purescript      ; javascript, but functional
+       ;python          ; beautiful is better than ugly
        ;rest            ; Emacs as a REST client
        ;ruby            ; 1.step do {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;rust            ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;scala           ; java, but good
-       sh              ; she sells (ba|z)sh shells on the C xor
+       ;sh              ; she sells (ba|z)sh shells on the C xor
        ;swift           ; who asked for emoji variables?
-       typescript      ; javascript, but better
-       web             ; the tubes
+       ;typescript      ; javascript, but better
+       ;web             ; the tubes
 
        :org
        org             ; organize your plain life in plain text
@@ -117,17 +118,20 @@
       ;org-sync        ; TODO sync with mobile
       ;org-publish     ; TODO org + blogs
 
-       ;; Applications are opinionated modules that transform Emacs to fulfill a
-       ;; specific purpose. They should be loaded last.
+       ;; Applications are complex and opinionated modules that transform Emacs
+       ;; toward a specific purpose. They may have additional dependencies and
+       ;; should be loaded last.
        :app
-      ;email           ; Emacs as an email client
-      ;irc             ; TODO how neckbeards socialize
-      ;rss             ; emacs as an RSS reader
-      ;twitter         ; twitter client https://twitter.com/vnought
+       email           ; emacs as an email client
+       irc             ; how neckbeards socialize
+       rss             ; emacs as an RSS reader
+       twitter         ; twitter client https://twitter.com/vnought
        write           ; emacs as a word processor (latex + org + markdown)
 
-       ;; Private modules are aren't tracked in the repo, except for mine. Use
-       ;; it as a reference for your own.
+       ;; Private modules named after your username are loaded automatically.
+       ;; Leaving this here is harmless though. Also, they are omitted from
+       ;; source control (except for mine; use it as a reference).
        :private
        hlissner
        ptival)
+
