8886cb7b0bd7bb105e16aa6d0d543fe9ba675df2
Valentin Robert <valentin.robert.42@gmail.com>
2020-10-28 12:20:47 +0100

    add hydra caches

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -57,6 +57,15 @@ in
       # }}"
     ];
 
+  nix = {
+    binaryCaches = [
+      "http://hydra.iohk.io/"
+    ];
+    binaryCachePublicKeys = [
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    ];
+  };
+
   nixpkgs = {
 
     # config = {
